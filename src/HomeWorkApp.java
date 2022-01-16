197e356e4b1ad8e1c4defea99e23dcf2e13703bb
Ekaterina Chueva <chuevaks@gmail.com>
2022-01-16 13:25:43 +0100

    hw for lesson2

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,24 +1,45 @@
 import static lesson1.hw1.*;
+import static lesson2.hw2.*;
 
 public class HomeWorkApp {
     // 1. Создать пустой проект в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
     public static void main(String[] args) {
 
+        //lesson 1
+
+//        taskNumber(1);
+//        printThreeWords();
+//
+//        taskNumber(2);
+//        checkSumSign(9, 5);
+//        checkSumSign(-9, 5);
+//
+//        taskNumber(3);
+//        printColor(0);
+//        printColor(50);
+//        printColor(150);
+//
+//        taskNumber(4);
+//        compareNumbers(10, 20);
+//        compareNumbers(20, 10);
+
+        //lesson 2
+
         taskNumber(1);
-        printThreeWords();
+        System.out.println(compareSum(5,4));
 
         taskNumber(2);
-        checkSumSign(9, 5);
-        checkSumSign(-9, 5);
+        checkNumbers(10);
 
         taskNumber(3);
-        printColor(0);
-        printColor(50);
-        printColor(150);
+        System.out.println(checkNumbersBool(8));
 
         taskNumber(4);
-        compareNumbers(10, 20);
-        compareNumbers(20, 10);
+        printString("Напечатай меня 5 раз",5);
+
+        taskNumber(5);
+        System.out.println(leapYear(2020));
+
 
 
     }
