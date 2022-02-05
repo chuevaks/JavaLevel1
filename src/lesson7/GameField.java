331304005975afad71e9f3590c52fb0056ec4413
Ekaterina Chueva <chuevaks@gmail.com>
2022-02-05 11:57:48 +0100

    hw for lesson7
    (cherry picked from commit 6d8e21a8789403c62e27f2b6f509948f50e213ba)

---
+++ src/lesson7/GameField.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+
+    GameField(){
+        setBackground(new Color(170,170,170));
+    }
+}
